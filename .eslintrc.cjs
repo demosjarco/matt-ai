67d93afc5ec4f8f39c47abee9400dff0e68aeb50
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-01-21 19:42:14 -0700

    Switch to warning

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -33,7 +33,7 @@ module.exports = {
 		'prefer-spread': 'off',
 		'no-case-declarations': 'off',
 		'no-console': 'off',
-		'@typescript-eslint/no-unused-vars': ['error'],
+		'@typescript-eslint/no-unused-vars': 'warn',
 		'@typescript-eslint/consistent-type-imports': 'warn',
 		'@typescript-eslint/no-unnecessary-condition': 'warn',
 		'qwik/jsx-img': 'off',
