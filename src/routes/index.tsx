1f39c65e7ebd07cfbc5b812579111dc59de86236
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-01-21 20:55:07 -0700

    Update under construction

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,12 +4,8 @@ import type { DocumentHead } from '@builder.io/qwik-city';
 export default component$(() => {
 	return (
 		<>
-			<h1>Hi 👋</h1>
-			<p>
-				Can't wait to see what you build with qwik!
-				<br />
-				Happy coding.
-			</p>
+			<h1>M.A.T.T. AI</h1>
+			<p>Coming soon!</p>
 		</>
 	);
 });
