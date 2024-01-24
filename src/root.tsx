9a46e7b4631eb6f64b7e2ffc071151a349ce1141
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-01-23 20:53:11 -0700

    Test typechat

--- src/root.tsx
+++ src/root.tsx
@@ -1,16 +1,25 @@
 import { component$, useServerData, useVisibleTask$ } from '@builder.io/qwik';
-import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
+import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister, server$ } from '@builder.io/qwik-city';
 import { initFlowbite } from 'flowbite';
 import { FaStylesheet } from 'qwik-fontawesome';
 import { RouterHead } from './components/router-head/router-head';
 
 import './global.less';
+import { TypeChatTest } from './typechatTest.mjs';
+
+export const simpleServerCall = server$(async function () {
+	await new TypeChatTest(this.platform).testing();
+});
 
 export default component$(() => {
 	const nonce = useServerData<string | undefined>('nonce');
 
 	useVisibleTask$(() => {
 		initFlowbite();
+	});
+
+	useVisibleTask$(async () => {
+		await simpleServerCall();
 	});
 
 	return (
