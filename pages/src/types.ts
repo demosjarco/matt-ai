df2aedf1231ad9aa5f4dca95d05c20e701313820
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-01-25 20:14:23 -0700

    Add types

--- pages/src/types.ts
+++ pages/src/types.ts
@@ -12,3 +12,25 @@ interface PagesEnvironmentvariables {
 	CF_PAGES_BRANCH: string;
 	CF_PAGES_URL: string;
 }
+
+export interface IDBConversation {
+	id: number;
+	name: string;
+	atime: Date;
+	btime: Date;
+	ctime: Date;
+}
+
+export interface IDBMessage {
+	id: number;
+	conversation_id: number;
+	content_version: number;
+	btime: Date;
+	role: 'system' | 'user' | 'assistant';
+	model_used: string;
+	content_message: string;
+	content_supplemental: Record<string, any>[];
+	content_cards: Record<string, any>[];
+	content_chips: Record<string, any>[];
+	content_references: Record<string, any>[];
+}
