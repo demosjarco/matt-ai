8045a668ff964e1a146c15a02de27d6a7824e16c
George Portillo <georgeportillodesign@gmail.com>
2024-01-28 14:08:33 -0800

    Fix close button

--- pages/src/components/sidebar/index.tsx
+++ pages/src/components/sidebar/index.tsx
@@ -5,7 +5,7 @@ import History from './history';
 
 export default component$(() => (
 	<aside id="default-sidebar" class="fixed left-0 top-0 z-40 h-screen w-64 -translate-x-full transition-transform sm:translate-x-0" aria-label="Sidebar">
-		<button type="button" data-drawer-hide="drawer-backdrop" aria-controls="drawer-backdrop" class="absolute end-2.5 top-2.5 inline-flex h-8 w-8 items-center justify-center rounded-lg bg-transparent text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white">
+		<button type="button" data-drawer-hide="default-sidebar" aria-controls="default-sidebar" class="absolute end-2.5 top-2.5 inline-flex h-8 w-8 items-center justify-center rounded-lg bg-transparent text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white">
 			<svg class="h-3 w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
 				<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
 			</svg>
