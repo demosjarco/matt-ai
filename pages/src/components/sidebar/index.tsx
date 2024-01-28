e8113401c6c2bff016f13e2a33f8e8927e895c6d
George Portillo <georgeportillodesign@gmail.com>
2024-01-28 14:06:21 -0800

    Add drawer init

--- pages/src/components/sidebar/index.tsx
+++ pages/src/components/sidebar/index.tsx
@@ -4,11 +4,19 @@ import Controls from './controls';
 import History from './history';
 
 export default component$(() => (
-	<aside id="separator-sidebar" class="fixed left-0 top-0 z-40 h-screen w-64 -translate-x-full transition-transform sm:translate-x-0" aria-label="Sidebar">
-		<div class="h-full overflow-y-auto bg-gray-50 px-3 py-4 dark:bg-gray-800">
+	<aside id="default-sidebar" class="fixed left-0 top-0 z-40 h-screen w-64 -translate-x-full transition-transform sm:translate-x-0" aria-label="Sidebar">
+		<button type="button" data-drawer-hide="drawer-backdrop" aria-controls="drawer-backdrop" class="absolute end-2.5 top-2.5 inline-flex h-8 w-8 items-center justify-center rounded-lg bg-transparent text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white">
+			<svg class="h-3 w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+				<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+			</svg>
+			<span class="sr-only">Close menu</span>
+		</button>
+		<div class="flex h-full flex-col justify-between overflow-y-auto bg-gray-50 px-3 py-4 dark:bg-gray-800">
 			<History />
-			<Banner />
-			<Controls />
+			<div>
+				<Banner />
+				<Controls />
+			</div>
 		</div>
 	</aside>
 ));
--- pages/src/routes/index.tsx
+++ pages/src/routes/index.tsx
@@ -1,8 +1,13 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useVisibleTask$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
+import { initDrawers } from 'flowbite';
 import Sidebar from '../components/sidebar';
 
 export default component$(() => {
+	useVisibleTask$(() => {
+		initDrawers();
+	});
+
 	return (
 		<>
 			<button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" class="ms-3 mt-2 inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 sm:hidden dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
