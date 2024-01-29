520246f3c0e46564315976cdec1de868aba0a232
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-01-29 12:04:29 -0700

    Replace with fontawesome

--- pages/src/components/chat/Chat.tsx
+++ pages/src/components/chat/Chat.tsx
@@ -1,6 +1,7 @@
 import { $, component$, useSignal, useVisibleTask$ } from '@builder.io/qwik';
 import { Form, type DocumentHead } from '@builder.io/qwik-city';
 import { faPaperPlane } from '@fortawesome/free-regular-svg-icons';
+import { faPaperclip } from '@fortawesome/free-solid-svg-icons';
 import { FaIcon } from 'qwik-fontawesome';
 import { sendMessage, useConversationId, useCreateConversation } from '../../routes/layout';
 import Message from './Message';
@@ -51,9 +52,7 @@ export default component$(() => {
 					<div class="flex h-16 w-full flex-row items-center bg-white p-2 dark:bg-slate-800">
 						<div>
 							<button class="flex items-center justify-center text-gray-400 hover:text-gray-600">
-								<svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-									<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"></path>
-								</svg>
+								<FaIcon icon={faPaperclip} />
 							</button>
 						</div>
 						<div class="ml-4 flex-grow">
@@ -88,9 +87,7 @@ export default component$(() => {
 					<Form action={createConversation} class="flex h-16 w-full flex-row items-center bg-white p-2 dark:bg-slate-800">
 						<div>
 							<button class="flex items-center justify-center text-gray-400 hover:text-gray-600">
-								<svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-									<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"></path>
-								</svg>
+								<FaIcon icon={faPaperclip} />
 							</button>
 						</div>
 						<div class="ml-4 flex-grow">
--- pages/src/components/sidebar/banner.tsx
+++ pages/src/components/sidebar/banner.tsx
@@ -6,9 +6,7 @@ export default component$(() => (
 			<span class="me-2 rounded bg-orange-100 px-2.5 py-0.5 text-sm font-semibold text-orange-800 dark:bg-orange-200 dark:text-orange-900">Beta</span>
 			{/* <button type="button" class="-mx-1.5 -my-1.5 ms-auto inline-flex h-6 h-6 w-6 w-6 items-center justify-center rounded-lg bg-blue-50 p-1 text-blue-900 hover:bg-blue-200 focus:ring-2 focus:ring-blue-400 dark:bg-blue-900 dark:text-blue-400 dark:hover:bg-blue-800" data-dismiss-target="#dropdown-cta" aria-label="Close">
 				<span class="sr-only">Close</span>
-				<svg class="h-2.5 w-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-					<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-				</svg>
+				<FaIcon icon={faXmark} />
 			</button> */}
 		</div>
 		<p class="mb-3 text-sm text-blue-800 dark:text-blue-400">M.A.T.T. (Magically All The Things) is currently in a BETA phase. This open source project is meant to showcase everything possible with Workers AI, seamlessly in 1 place.</p>
