3a3b8d653ae5fe991bab20842c2049136c8ece4b
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-01-29 17:38:15 -0700

    Add middleware for checking turnstile

---
+++ pages/src/routes/c/layout.tsx
@@ -0,0 +1,46 @@
+import type { RequestHandler } from '@builder.io/qwik-city';
+import type { ChatFormSubmit, EnvVars } from '../../types';
+
+export const onPost: RequestHandler = async ({ platform, request, parseBody, send }) => {
+	const incomingFormData = (await parseBody()) as ChatFormSubmit;
+	console.debug('Turnstile middleware', 'Incoming Form Data', incomingFormData);
+
+	if (incomingFormData) {
+		if (incomingFormData['cf-turnstile-response']) {
+			const ip = request.headers.get('CF-Connecting-IP');
+
+			const formData = new FormData();
+			formData.append('secret', (platform.env as EnvVars).TURNSTILE_SECRET_KEY);
+			formData.append('response', incomingFormData['cf-turnstile-response']);
+			if (ip) formData.append('remoteip', ip);
+
+			let turnstileSuccess: boolean = false;
+
+			try {
+				const result = await fetch(new URL('https://challenges.cloudflare.com/turnstile/v0/siteverify'), {
+					method: 'POST',
+					body: formData,
+				});
+				const outcome: { success: boolean } = await result.json();
+				turnstileSuccess = outcome.success;
+			} catch (error) {
+				console.error('Turnstile verify fail', error);
+				throw send(500, 'Internal Server Error');
+			}
+
+			console.debug('Turnstile check', turnstileSuccess);
+
+			if (!turnstileSuccess) {
+				throw send(403, 'Forbidden');
+			}
+		} else {
+			// Turnstile not present
+			console.error('No turnstile present', incomingFormData['cf-turnstile-response']);
+			throw send(403, 'Forbidden');
+		}
+	} else {
+		// Not valid form
+		console.error('Bad form data', incomingFormData);
+		throw send(400, 'Bad Request');
+	}
+};
