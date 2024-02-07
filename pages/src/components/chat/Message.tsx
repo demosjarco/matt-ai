b4007f75289d82ddca7315f2bb167c032fd59a99
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-02-07 00:47:03 -0700

    Render image if present

--- pages/src/components/chat/Message.tsx
+++ pages/src/components/chat/Message.tsx
@@ -1,4 +1,6 @@
 import { component$ } from '@builder.io/qwik';
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import { FaIcon } from 'qwik-fontawesome';
 import type { IDBMessage } from '../../types';
 import Avatar from './Avatar';
 
@@ -23,6 +25,20 @@ export default component$((props: { message: IDBMessage; userLocale?: string }) 
 								{/* <pre class="whitespace-pre-wrap text-balance">{JSON.stringify(props.message, null, '\t')}</pre> */}
 								{props.message.content[props.message.content.findIndex((record) => 'text' in record)]?.text?.trim() ?? ''}
 							</p>
+							{props.message.content.findIndex((record) => 'image' in record) >= 0 ? (
+								<div class="group relative my-2.5">
+									<div class="absolute flex h-full w-full items-center justify-center rounded-lg bg-gray-900/50 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+										<button data-tooltip-target="download-image" class="inline-flex h-10 w-10 items-center justify-center rounded-full bg-white/30 hover:bg-white/50 focus:outline-none focus:ring-4 focus:ring-gray-50 dark:text-white">
+											<FaIcon icon={faDownload} />
+										</button>
+										<div id="download-image" role="tooltip" class="tooltip invisible absolute z-10 inline-block rounded-lg bg-gray-900 px-3 py-2 text-sm font-medium text-white opacity-0 shadow-sm transition-opacity duration-300 dark:bg-gray-700">
+											Download image
+											<div class="tooltip-arrow" data-popper-arrow></div>
+										</div>
+									</div>
+									<img src={URL.createObjectURL(new Blob([props.message.content[props.message.content.findIndex((record) => 'image' in record)]!.image!], { type: 'image/png' }))} class="rounded-lg" />
+								</div>
+							) : undefined}
 						</div>
 						{isMe ? undefined : <span class={`flex text-sm font-normal text-gray-500 dark:text-gray-400 ${isMe ? 'justify-end' : ''}`}>{Array.isArray(props.message.status) ? props.message.status.join(', ') : props.message.status ? 'Delivered' : 'Sent'}</span>}
 					</div>
