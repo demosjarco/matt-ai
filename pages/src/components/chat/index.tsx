ed523316f1d3b750f32d0f3e82027d4018a02b0c
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-03-29 20:46:47 -0700

    Fix the input box

--- pages/src/components/chat/index.tsx
+++ pages/src/components/chat/index.tsx
@@ -291,8 +291,19 @@ export default component$(() => {
 					</div>
 					<div class="ml-4 flex-grow">
 						<div class="relative flex w-full flex-row flex-nowrap">
-							<input type="text" id="floating_outlined" class="border-1 peer mt-3 block h-10 w-full appearance-none rounded-xl border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-blue-500" placeholder=" " />
-							<label for="floating_outlined" class="absolute start-1 top-2 z-10 mt-3 origin-[0] -translate-y-3 scale-75 transform bg-white px-2 text-sm text-gray-500 duration-300 peer-placeholder-shown:top-1/3 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-3 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-blue-600 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4 dark:bg-gray-800 dark:text-gray-400 peer-focus:dark:text-blue-500">
+							<input
+								id="floating_outlined"
+								class="border-1 peer block h-10 w-full appearance-none rounded-xl border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0 dark:border-gray-600 dark:text-white dark:focus:border-blue-500"
+								name="message"
+								type="text"
+								placeholder=" "
+								spellcheck={true}
+								autoCapitalize="on"
+								// @ts-ignore
+								autocorrect="on"
+								enterKeyHint="send"
+							/>
+							<label for="floating_outlined" class="absolute start-1 top-2 z-10 origin-[0] -translate-y-3 scale-75 transform bg-white px-2 text-sm text-gray-500 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-3 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-blue-600 rtl:peer-focus:left-auto rtl:peer-focus:translate-x-1/4 dark:bg-gray-800 dark:text-gray-400 peer-focus:dark:text-blue-500">
 								Ask me anything
 							</label>
 							{isLocal.value ? <div class="cf-turnstile flex h-16 flex-none" data-sitekey="1x00000000000000000000BB" data-action="send-chat-message" data-appearance="interaction-only"></div> : <div class="cf-turnstile flex h-16 flex-none" data-sitekey="0x4AAAAAAAQ34m_klLCEVN51" data-action="send-chat-message" data-appearance="interaction-only"></div>}
