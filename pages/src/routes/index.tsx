7bebd814611170d20f38ff4eb4f6a7ee752b9810
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-04-03 20:01:11 -0700

    Fix headers

--- pages/src/routes/index.tsx
+++ pages/src/routes/index.tsx
@@ -10,12 +10,49 @@ export default component$(() => {
 	);
 });
 
-export const head: DocumentHead = {
-	title: 'M.A.T.T. AI',
-	meta: [
-		{
-			name: 'description',
-			content: 'Qwik site description',
-		},
-	],
+export const head: DocumentHead = ({ url }) => {
+	const title = 'M.A.T.T. AI';
+	const description = 'Magically All The Things AI';
+
+	return {
+		title,
+		meta: [
+			{
+				name: 'description',
+				content: description,
+			},
+			{
+				name: 'theme-color',
+				media: '#1E293B',
+			},
+			/**
+			 * OpenGraph
+			 * @url https://ogp.me/
+			 */
+			{
+				property: 'og:title',
+				content: title,
+			},
+			{
+				property: 'og:type',
+				content: 'website',
+			},
+			{
+				property: 'og:url',
+				content: url.toString(),
+			},
+			{
+				name: 'og:description',
+				content: description,
+			},
+			{
+				name: 'og:locale',
+				content: 'en_US',
+			},
+			{
+				name: 'og:site_name ',
+				content: title,
+			},
+		],
+	};
 };
