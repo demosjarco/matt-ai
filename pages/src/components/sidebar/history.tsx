b86caff737f62da9f1399dbd392cc0d2f932e7c4
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-04-07 00:50:18 -0700

    Fix scrolling issue for conversation list

--- pages/src/components/sidebar/history.tsx
+++ pages/src/components/sidebar/history.tsx
@@ -26,7 +26,7 @@ export default component$(() => {
 	});
 
 	return (
-		<ul class="h-full space-y-2 overflow-y-auto font-medium">
+		<ul class="space-y-2 font-medium">
 			{conversations.value.map((conversation) => (
 				<Item
 					onClick$={(id) => {
