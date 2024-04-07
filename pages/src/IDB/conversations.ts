bb195d125f7f9ca035faa7ffb9a2721b65f5b60f
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-04-07 00:26:42 -0700

    Get individual conversation

--- pages/src/IDB/conversations.ts
+++ pages/src/IDB/conversations.ts
@@ -1,8 +1,24 @@
 import { IDBBase } from './base';
+import type { IDBConversation } from './schemas/v2';
+
+type InternalConversationGuarantee = Pick<IDBConversation, 'key'> & Omit<Partial<IDBConversation>, 'key'>;
 
 export class IDBConversations extends IDBBase {
 	public get conversations() {
 		// Get all conversations, but sorted newest to oldest
 		return this.db.then((db) => db.getAll('conversations').then((conversations) => conversations.sort((a, b) => b.mtime.getTime() - a.mtime.getTime())));
+	}
+
+	public getConversation(conversation: InternalConversationGuarantee) {
+		return new Promise<IDBConversation>((resolve, reject) =>
+			this.db
+				.then((db) =>
+					db
+						.get('conversations', conversation.key!)
+						.then((value) => (value ? resolve(value) : reject()))
+						.catch(reject),
+				)
+				.catch(reject),
+		);
 	}
 }
