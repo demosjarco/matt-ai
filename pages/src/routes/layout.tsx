9440a0181399e732d3d7a329c80b637aab4dcdc3
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-04-10 22:05:47 -0700

    Update layout.tsx

--- pages/src/routes/layout.tsx
+++ pages/src/routes/layout.tsx
@@ -5,17 +5,6 @@ import { FaBarsSolid } from '@qwikest/icons/font-awesome';
 import Sidebar from '../components/sidebar';
 import { runningLocally } from '../extras';
 
-export const onGet: RequestHandler = async ({ cacheControl }) => {
-	// Control caching for this request for best performance and to reduce hosting costs:
-	// https://qwik.builder.io/docs/caching/
-	cacheControl({
-		// Always serve a cached response by default, up to a week stale
-		staleWhileRevalidate: 60 * 60 * 24 * 7,
-		// Max once every 5 seconds, revalidate on the server to get a fresh version of this page
-		maxAge: 5,
-	});
-};
-
 export const useConversationId = routeLoader$(({ params }) => {
 	const conversationId = Number(params['conversationId']);
 	return isNaN(conversationId) ? undefined : conversationId;
@@ -68,6 +57,17 @@ export const head: DocumentHead = {
 			content: 'M.A.T.T.',
 		},
 	],
+};
+
+export const onGet: RequestHandler = async ({ cacheControl }) => {
+	// Control caching for this request for best performance and to reduce hosting costs:
+	// https://qwik.builder.io/docs/caching/
+	cacheControl({
+		// Always serve a cached response by default, up to a week stale
+		staleWhileRevalidate: 60 * 60 * 24 * 7,
+		// Max once every 5 seconds, revalidate on the server to get a fresh version of this page
+		maxAge: 5,
+	});
 };
 
 export default component$(() => {
