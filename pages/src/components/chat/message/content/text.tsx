0f2222f31d28c30d91b3de57de2c66c149154182
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-04-10 23:21:38 -0700

    Test dynamic full row

--- pages/src/components/chat/message/content/text.tsx
+++ pages/src/components/chat/message/content/text.tsx
@@ -6,10 +6,10 @@ export default component$((props: { text?: IDBMessageContentText }) => {
 		return <p class="whitespace-pre-wrap text-balance text-sm font-normal text-gray-900 dark:text-white">{props.text.trim()}</p>;
 	} else {
 		return (
-			<div role="status" class="max-w-lg animate-pulse space-y-2.5">
+			<div role="status" class="w-full animate-pulse space-y-2.5">
 				<div class="flex w-full items-center">
-					<div class="h-2.5 w-32 rounded-full bg-gray-200 dark:bg-gray-700"></div>
-					<div class="ms-2 h-2.5 w-24 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+					<div class="h-2.5 w-1/4 rounded-full bg-gray-200 dark:bg-gray-700"></div>
+					<div class="ms-2 h-2.5 w-1/5 rounded-full bg-gray-300 dark:bg-gray-600"></div>
 					<div class="ms-2 h-2.5 w-full rounded-full bg-gray-300 dark:bg-gray-600"></div>
 				</div>
 				<div class="flex w-full max-w-[480px] items-center">
