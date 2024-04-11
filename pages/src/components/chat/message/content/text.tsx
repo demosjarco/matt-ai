b6b85c82486aaed077e6e5f5685d6d8a10e678ed
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-04-10 23:58:55 -0700

    Fix colors on placeholders

--- pages/src/components/chat/message/content/text.tsx
+++ pages/src/components/chat/message/content/text.tsx
@@ -8,34 +8,34 @@ export default component$((props: { text?: IDBMessageContentText }) => {
 		return (
 			<div role="status" class="w-full animate-pulse space-y-2.5">
 				<div class="flex w-full items-center">
-					<div class="h-2.5 w-1/4 rounded-full bg-gray-200 dark:bg-gray-700"></div>
-					<div class="ms-2 h-2.5 w-1/5 rounded-full bg-gray-300 dark:bg-gray-600"></div>
-					<div class="ms-2 h-2.5 w-full rounded-full bg-gray-300 dark:bg-gray-600"></div>
+					<div class="h-2.5 w-1/4 rounded-full bg-gray-200 dark:bg-gray-600"></div>
+					<div class="ms-2 h-2.5 w-1/5 rounded-full bg-gray-300 dark:bg-gray-500"></div>
+					<div class="ms-2 h-2.5 w-full rounded-full bg-gray-300 dark:bg-gray-500"></div>
 				</div>
 				<div class="flex w-11/12 items-center">
-					<div class="h-2.5 w-full rounded-full bg-gray-200 dark:bg-gray-700"></div>
-					<div class="ms-2 h-2.5 w-full rounded-full bg-gray-300 dark:bg-gray-600"></div>
-					<div class="ms-2 h-2.5 w-1/5 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+					<div class="h-2.5 w-full rounded-full bg-gray-200 dark:bg-gray-600"></div>
+					<div class="ms-2 h-2.5 w-full rounded-full bg-gray-300 dark:bg-gray-500"></div>
+					<div class="ms-2 h-2.5 w-1/5 rounded-full bg-gray-300 dark:bg-gray-500"></div>
 				</div>
 				<div class="flex w-4/5 items-center">
-					<div class="h-2.5 w-full rounded-full bg-gray-300 dark:bg-gray-600"></div>
-					<div class="ms-2 h-2.5 w-4/5 rounded-full bg-gray-200 dark:bg-gray-700"></div>
-					<div class="ms-2 h-2.5 w-full rounded-full bg-gray-300 dark:bg-gray-600"></div>
+					<div class="h-2.5 w-full rounded-full bg-gray-300 dark:bg-gray-500"></div>
+					<div class="ms-2 h-2.5 w-4/5 rounded-full bg-gray-200 dark:bg-gray-600"></div>
+					<div class="ms-2 h-2.5 w-full rounded-full bg-gray-300 dark:bg-gray-500"></div>
 				</div>
 				<div class="flex w-11/12 items-center">
-					<div class="ms-2 h-2.5 w-full rounded-full bg-gray-200 dark:bg-gray-700"></div>
-					<div class="ms-2 h-2.5 w-full rounded-full bg-gray-300 dark:bg-gray-600"></div>
-					<div class="ms-2 h-2.5 w-1/5 rounded-full bg-gray-300 dark:bg-gray-600"></div>
+					<div class="ms-2 h-2.5 w-full rounded-full bg-gray-200 dark:bg-gray-600"></div>
+					<div class="ms-2 h-2.5 w-full rounded-full bg-gray-300 dark:bg-gray-500"></div>
+					<div class="ms-2 h-2.5 w-1/5 rounded-full bg-gray-300 dark:bg-gray-500"></div>
 				</div>
 				<div class="flex w-5/6 items-center">
-					<div class="ms-2 h-2.5 w-1/4 rounded-full bg-gray-300 dark:bg-gray-600"></div>
-					<div class="ms-2 h-2.5 w-1/5 rounded-full bg-gray-300 dark:bg-gray-600"></div>
-					<div class="ms-2 h-2.5 w-full rounded-full bg-gray-200 dark:bg-gray-700"></div>
+					<div class="ms-2 h-2.5 w-1/4 rounded-full bg-gray-300 dark:bg-gray-500"></div>
+					<div class="ms-2 h-2.5 w-1/5 rounded-full bg-gray-300 dark:bg-gray-500"></div>
+					<div class="ms-2 h-2.5 w-full rounded-full bg-gray-200 dark:bg-gray-600"></div>
 				</div>
 				<div class="flex w-2/3 items-center">
-					<div class="ms-2 h-2.5 w-full rounded-full bg-gray-300 dark:bg-gray-600"></div>
-					<div class="ms-2 h-2.5 w-11/12 rounded-full bg-gray-200 dark:bg-gray-700"></div>
-					<div class="ms-2 h-2.5 w-full rounded-full bg-gray-300 dark:bg-gray-600"></div>
+					<div class="ms-2 h-2.5 w-full rounded-full bg-gray-300 dark:bg-gray-500"></div>
+					<div class="ms-2 h-2.5 w-11/12 rounded-full bg-gray-200 dark:bg-gray-600"></div>
+					<div class="ms-2 h-2.5 w-full rounded-full bg-gray-300 dark:bg-gray-500"></div>
 				</div>
 				<span class="sr-only">Loading...</span>
 			</div>
