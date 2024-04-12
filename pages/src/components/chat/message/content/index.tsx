0db2d446a78285dc0d4b62983082244c951aa87f
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-04-11 19:14:24 -0700

    More accurate loading

--- pages/src/components/chat/message/content/index.tsx
+++ pages/src/components/chat/message/content/index.tsx
@@ -15,8 +15,8 @@ export default component$((props: { message: IDBMessage }) => {
 
 	return (
 		<div class={`leading-1.5 relative flex flex-col border-gray-200 bg-gray-100 p-4 dark:bg-gray-700 ${isMe ? 'rounded-xl rounded-se-none' : 'rounded-e-xl rounded-es-xl'}`}>
-			{(props.message.status as Exclude<IDBMessage['status'], boolean>).indexOf('typing') > -1 || textContentIndex >= 0 ? <Text text={props.message.content[textContentIndex]?.text} /> : undefined}
-			{(props.message.status as Exclude<IDBMessage['status'], boolean>).indexOf('imageGenerating') > -1 || imageContentIndex >= 0 ? <Image imageAction={props.message.content[actionIndex]?.action?.imageGenerate ?? undefined} image={props.message.content[imageContentIndex]?.image} /> : undefined}
+			{(Array.isArray(props.message.status) && (props.message.status as Exclude<IDBMessage['status'], boolean>).indexOf('typing') > -1) || textContentIndex >= 0 ? <Text text={props.message.content[textContentIndex]?.text} /> : undefined}
+			{(Array.isArray(props.message.status) && (props.message.status as Exclude<IDBMessage['status'], boolean>).indexOf('imageGenerating') > -1) || imageContentIndex >= 0 ? <Image imageAction={props.message.content[actionIndex]?.action?.imageGenerate ?? undefined} image={props.message.content[imageContentIndex]?.image} /> : undefined}
 			{cardContentIndex >= 0 ? <Card card={props.message.content[cardContentIndex]?.card} /> : undefined}
 			{props.message.safe !== undefined && props.message.safe !== true ? <SafetyBanner knownBad={props.message.safe !== null} /> : undefined}
 		</div>
