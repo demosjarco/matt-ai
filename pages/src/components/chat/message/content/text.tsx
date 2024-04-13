5579afae7b5684180c4039b19d9cc6745feb0004
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-04-13 01:34:29 -0700

    more debug logging

--- pages/src/components/chat/message/content/text.tsx
+++ pages/src/components/chat/message/content/text.tsx
@@ -18,6 +18,7 @@ export default component$<{ id: IDBMessage['key'] }>((props) => {
 		track(() => divRef.value);
 		track(() => pRef.value);
 
+		console.debug('TextElement', text);
 		if (text) {
 			if (divRef.value) {
 				try {
