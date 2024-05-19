2cce2d18e6148e68f9055f2619eb3509c3c56855
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-05-18 22:45:58 -0700

    Make it a store for `deep`

--- pages/src/extras/context.tsx
+++ pages/src/extras/context.tsx
@@ -1,5 +1,5 @@
-import { createContextId, type Signal } from '@builder.io/qwik';
+import { createContextId } from '@builder.io/qwik';
 import type { IDBConversation, IDBMessage } from '../IDB/schemas/v2';
 
-export const ConversationsContext = createContextId<Signal<IDBConversation[]>>('ConversationsContext');
+export const ConversationsContext = createContextId<IDBConversation[]>('ConversationsContext');
 export const MessagesContext = createContextId<Record<NonNullable<IDBMessage['key']>, IDBMessage>>('MessagesContext');
