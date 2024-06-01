0030ab6cc1930ad1385b6d5ebd1debb9b3341d5b
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-06-01 18:44:59 -0400

    Delete SentimentResponse.ts

--- worker/aiTypes/SentimentResponse.ts
+++
@@ -1,5 +0,0 @@
-// The following is a schema definition for determining the sentiment of a some user input.
-
-export interface SentimentResponse {
-	sentiment: 'negative' | 'neutral' | 'positive'; // The sentiment of the text
-}
