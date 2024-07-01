3aa20ab806cdf3099d9e19e0482123d1096dcd64
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-07-01 00:57:23 -0700

    Fix types

--- pages/src/entry.cloudflare-pages.tsx
+++ pages/src/entry.cloudflare-pages.tsx
@@ -1,11 +1,13 @@
 import { createQwikCity, type PlatformCloudflarePages } from '@builder.io/qwik-city/middleware/cloudflare-pages';
+import type { Request } from '@cloudflare/workers-types/experimental';
 import qwikCityPlan from '@qwik-city-plan';
 import { manifest } from '@qwik-client-manifest';
 import render from './entry.ssr';
 import type { EnvVars } from './types';
 
 declare global {
-	interface QwikCityPlatform extends PlatformCloudflarePages {
+	interface QwikCityPlatform extends Omit<PlatformCloudflarePages, 'request'> {
+		request: Request;
 		env: EnvVars;
 	}
 }
