010bb1bd26d0476e93ddf4a64de2d8e6dd1f3b5e
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-07-10 10:49:22 -0700

    Added new types

--- worker/src/types.ts
+++ worker/src/types.ts
@@ -11,4 +11,9 @@ interface Bindings {
 }
 
 export type modelTypes = keyof typeof workersAiCatalog.modelGroups;
-export type modelPossibilities<M extends modelTypes = modelTypes> = (typeof workersAiCatalog.modelGroups)[M]['models'][number]['name'];
+export type modelPossibilitiesRaw<M extends modelTypes = modelTypes> = (typeof workersAiCatalog.modelGroups)[M]['models'][number];
+export type modelPossibilities<M extends modelTypes = modelTypes> = modelPossibilitiesRaw<M>['name'];
+type modelProperties<Model> = Model extends { properties: infer Props } ? keyof Props : never;
+type modelPossibilitiesProperties<M extends modelTypes = modelTypes> = modelProperties<modelPossibilitiesRaw<M>>;
+export type filteredModelPossibilitiesRaw<M extends modelTypes = modelTypes, K extends modelPossibilitiesProperties<M> = modelPossibilitiesProperties<M>, V extends modelPossibilitiesRaw<M>['properties'][K] = any> = modelPossibilitiesRaw<M> extends infer Model ? (Model extends { properties: Record<K, V> } ? Model : never) : never;
+export type filteredModelPossibilities<M extends modelTypes = modelTypes, K extends modelPossibilitiesProperties<M> = modelPossibilitiesProperties<M>, V extends modelPossibilitiesRaw<M>['properties'][K] = any> = filteredModelPossibilitiesRaw<M, K, V>['name'];
