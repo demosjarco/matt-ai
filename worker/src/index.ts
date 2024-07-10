0c508de01c26f9215089a4ccb1bd2ef104bc5544
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-07-10 12:29:48 -0700

    Move to separate function

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -49,90 +49,99 @@ export default class extends WorkerEntrypoint<EnvVars> {
 			(c) => {
 				const { req, res } = c.req.valid('form');
 
-				/**
-				 * It's `LLMClassifierFromTemplate` from `autoevals` but withotu openai hardcoding
-				 */
-				const useCoT: boolean = true;
-				// @ts-ignore
-				return this.env.AI.run('@hf/nousresearch/hermes-2-pro-mistral-7b' satisfies filteredModelPossibilities<'Text Generation', 'function_calling', true>, {
-					// Financial safety limit
-					max_tokens: 512,
-					// Works better
-					temperature: 0.2,
-					messages: [
-						{ role: 'system', content: `You are an peer-review evaluator for AI responses. Evaluate the following response and associated request using the tools as criteria.${useCoT ? ' Use your reasoning in a step-by-step manner to be sure that your conclusion is correct. Avoid simply stating the correct answer at the outset.' : ''}` },
-						{ role: 'user', content: req },
-						{ role: 'assistant', content: res },
-					],
-					tools: [
-						{
-							name: 'Relevance',
-							description: 'How relevant is the response to the request?',
-							parameters: {
-								type: 'object',
-								properties: {
-									score: {
-										type: 'integer',
-										description: 'The score is an integer from 0 to 100. 0 being completely irrelevant. 100 being perfect spot on relevant to user request',
-									},
-								},
-								required: ['score'],
-							},
-						},
-						{
-							name: 'Clarity',
-							description: 'How clear and understandable is the response?',
-							parameters: {
-								type: 'object',
-								properties: {
-									score: {
-										type: 'integer',
-										description: 'The score is an integer from 0 to 100. 0 being completely garbled and not human readable. 100 being perfect grammar and spelling',
-									},
-								},
-								required: ['score'],
-							},
-						},
-						{
-							name: 'Comprehensiveness',
-							description: 'How comprehensive is the response?',
-							parameters: {
-								type: 'object',
-								properties: {
-									score: {
-										type: 'integer',
-										description: 'The score is an integer from 0 to 100. 0 is like forgetting what the user even asked and answering differently. 100 is addressing all aspects of the assigned task, including depth, breadth, relevant details, and overall completeness.',
-									},
-								},
-								required: ['score'],
-							},
-						},
-						{
-							name: 'Hallucination/Accuracy',
-							description: 'Does the response contain any factual inaccuracies or hallucinations?',
-							parameters: {
-								type: 'object',
-								properties: {
-									score: {
-										type: 'integer',
-										description: 'The score is an integer from 0 to 100. 0 having the response filled with hallucinations. 100 being perfect spot on accuracy',
-									},
-								},
-								required: ['score'],
-							},
-						},
-					],
-				}).then((response) => {
-					const { tool_calls } = response as Exclude<AiTextGenerationOutput, ReadableStream>;
-					const grades = this.combineGrading(tool_calls as InferenceResult[]);
-					const finalGrade = 0.3 * grades.Relevance + 0.25 * grades.Clarity + 0.25 * grades.Comprehensiveness + 0.2 * grades['Hallucination/Accuracy'];
-
-					return c.json({ ...grades, finalGrade, willUseAsTrainingData: finalGrade >= 80 });
-				});
+				return this.aiGrade(req, res).then((grade) => c.json(grade));
 			},
 		);
 
 		return app.fetch(request, this.env, this.ctx);
+	}
+	public aiGrade(req: string, res: string) {
+		/**
+		 * It's `LLMClassifierFromTemplate` from `autoevals` but withotu openai hardcoding
+		 */
+		const useCoT: boolean = true;
+		// @ts-ignore
+		return this.env.AI.run('@hf/nousresearch/hermes-2-pro-mistral-7b' satisfies filteredModelPossibilities<'Text Generation', 'function_calling', true>, {
+			// Financial safety limit
+			max_tokens: 512,
+			// Works better
+			temperature: 0.2,
+			messages: [
+				{ role: 'system', content: `You are an peer-review evaluator for AI responses. Evaluate the following response and associated request using the tools as criteria.${useCoT ? ' Use your reasoning in a step-by-step manner to be sure that your conclusion is correct. Avoid simply stating the correct answer at the outset.' : ''}` },
+				{ role: 'user', content: req },
+				{ role: 'assistant', content: res },
+			],
+			tools: [
+				{
+					name: 'Relevance',
+					description: 'How relevant is the response to the request?',
+					parameters: {
+						type: 'object',
+						properties: {
+							score: {
+								type: 'integer',
+								description: 'The score is an integer from 0 to 100. 0 being completely irrelevant. 100 being perfect spot on relevant to user request',
+							},
+						},
+						required: ['score'],
+					},
+				},
+				{
+					name: 'Clarity',
+					description: 'How clear and understandable is the response?',
+					parameters: {
+						type: 'object',
+						properties: {
+							score: {
+								type: 'integer',
+								description: 'The score is an integer from 0 to 100. 0 being completely garbled and not human readable. 100 being perfect grammar and spelling',
+							},
+						},
+						required: ['score'],
+					},
+				},
+				{
+					name: 'Comprehensiveness',
+					description: 'How comprehensive is the response?',
+					parameters: {
+						type: 'object',
+						properties: {
+							score: {
+								type: 'integer',
+								description: 'The score is an integer from 0 to 100. 0 is like forgetting what the user even asked and answering differently. 100 is addressing all aspects of the assigned task, including depth, breadth, relevant details, and overall completeness.',
+							},
+						},
+						required: ['score'],
+					},
+				},
+				{
+					name: 'Hallucination/Accuracy',
+					description: 'Does the response contain any factual inaccuracies or hallucinations?',
+					parameters: {
+						type: 'object',
+						properties: {
+							score: {
+								type: 'integer',
+								description: 'The score is an integer from 0 to 100. 0 having the response filled with hallucinations. 100 being perfect spot on accuracy',
+							},
+						},
+						required: ['score'],
+					},
+				},
+			],
+		}).then((response) => {
+			const { tool_calls } = response as Exclude<AiTextGenerationOutput, ReadableStream>;
+			const grades = this.combineGrading(tool_calls as InferenceResult[]);
+			const finalGrade = 0.4 * grades.Relevance + 0.3 * grades.Clarity + 0.15 * grades.Comprehensiveness + 0.15 * grades['Hallucination/Accuracy'];
+			/**
+			 * relevance, accuracy, comprehensive, clarity
+			 * vic & george: 2x, 2x, 1.5x, 1x
+			 * chad: 40%, 30%, 15%, 15%
+			 * steven: 40%, 40%, 10%, 10%
+			 */
+
+			return { ...grades, finalGrade, willUseAsTrainingData: finalGrade >= 80 };
+		});
 	}
 	private combineGrading(results: InferenceResult[]) {
 		const scoreMap = results.reduce(
